2d5b49e4eb9820cf78f4f3a8f23408fd5d97546d
Deovrat Singh <148208948+imrude16@users.noreply.github.com>
2025-10-05 18:35:50 +0530

    implemented auth logic

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -6,10 +6,16 @@ import SelectField from "@/components/forms/SelectField"
 import { CountrySelectField } from "@/components/forms/CountrySelectField"
 import { INVESTMENT_GOALS, PREFERRED_INDUSTRIES, RISK_TOLERANCE_OPTIONS } from "@/lib/constants"
 import FooterLink from "@/components/forms/FooterLink"
+import { signUpWithEmail } from "@/lib/actions/auth.actions"
+import { useRouter } from "next/navigation"
+import { toast } from "sonner"
+
 
 
 
 const SignUp = () => {
+    const router = useRouter();
+
     const {
         register,
         handleSubmit,
@@ -30,10 +36,12 @@ const SignUp = () => {
 
     const onSubmit = async (data: SignUpFormData) => {
         try {
+            const result = await signUpWithEmail(data);
+            if(result.success)  router.push('/')
 
         } catch (e) {
             console.error(e);
-
+            toast.error('SignUp Failed', { description: e instanceof Error ? e.message : 'Failed to create an account.' });
         }
     }
 
